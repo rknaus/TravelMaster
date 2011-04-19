b84a2dee16e2af03323f6f0e3e2e5c357f3ac1b3
Ruben Knaus <ruben.knaus@gmx.ch>
2011-04-20 00:36:55 +0200

    Refactoring TimeTable ... not finished

--- src/main/java/ch/netgeek/travelmaster/route/TimeTable.java
+++ src/main/java/ch/netgeek/travelmaster/route/TimeTable.java
@@ -2,134 +2,82 @@ package ch.netgeek.travelmaster.route;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
+/**
+ * Represents a timetable belonging to a line for a source and destination
+ * station. The departure times are stored in a LinkedList (sorted ascending
+ * by departure time).
+ * 
+ * @author      Dieu P. Van, Ruben Knaus
+ * @version     0.1
+ */
 public class TimeTable {
 
-    private ListNode head;
-    private ListNode tail;
+    // variables declaration
+    LinkedList<Calendar> departures;
 
-    public ListNode newListNode(Calendar calendar) throws NullPointerException {
-        ListNode listNode = new ListNode();
-        listNode.setCalendar(calendar);
-        return listNode;
+    /**
+     * A new Linked List of Calendar elements gets created when the constructor
+     * gets called.
+     */
+    public TimeTable() {
+        departures = new LinkedList<Calendar>();
     }
 
-    public Calendar getNextDeparture(Calendar calendar) {
-        
-        int calendarTimeInSec = calendar.get(Calendar.HOUR) * 3600 
-                                + calendar.get(Calendar.MINUTE) * 60;
-        
-        ListNode currentNode = head;
-        while (currentNode != null) {
-            int currentDepartureInSec = currentNode.getCalendar().get(
-                    Calendar.HOUR) * 3600 + currentNode.getCalendar().get(
-                    Calendar.MINUTE) * 60;
-            if (currentDepartureInSec >= calendarTimeInSec) {
-                return currentNode.getCalendar();
+    /**
+     * Adds a departure (Calendar object) to the departures list. It stores the
+     * departure sorted by time (ascending)
+     * 
+     * @param departure             The departure time
+     */
+    public void addDeparture(Calendar departure) {
+        if (departures.isEmpty()) {
+            departures.add(departure);
+        } else {
+            Calendar index = departures.getFirst();
+            int indexValue = index.get(Calendar.HOUR) * 60 + index.get(Calendar.MINUTE);
+            int departureValue = departure.get(Calendar.HOUR) * 60 + departure.get(Calendar.MINUTE);
+            ListIterator<Calendar> iterator = departures.listIterator();
+            while (iterator.hasNext() && indexValue <= departureValue) {
+                index = iterator.next();
+                indexValue = index.get(Calendar.HOUR) * 60 + index.get(Calendar.MINUTE);
             }
-            currentNode = currentNode.getNext();
+            departures.add(iterator.nextIndex(), departure);
         }
-        
-        return head.getCalendar();
+    }
+
+    /**
+     * Returns the next departure time according to the given time. If the
+     * departures list is empty, null gets returned. If the given time is later
+     * than any departure times, the earliest departure time gets returned.
+     * 
+     * @param calendar              The wished departure time
+     * @return                      The next departure time
+     */
+    public Calendar getNextDeparture(Calendar departure) {
+        if (departures.isEmpty()) {
+            return null;
+        } else {
+            Calendar index = departures.getFirst();
+            int indexValue = index.get(Calendar.HOUR) * 60 + index.get(Calendar.MINUTE);
+            int departureValue = departure.get(Calendar.HOUR) * 60 + departure.get(Calendar.MINUTE);
+            ListIterator<Calendar> iterator = departures.listIterator();
+            while (iterator.hasNext() && indexValue <= departureValue) {
+                index = iterator.next();
+                indexValue = index.get(Calendar.HOUR) * 60 + index.get(Calendar.MINUTE);
+            }
+            if (indexValue < departureValue) {
+                return departures.getFirst();
+            } else {
+                return departures.get(iterator.nextIndex());
+            }
+        }
     }
 
     public ArrayList<Calendar> getDepartures() {
-        
-        ArrayList<Calendar> departures = new ArrayList<Calendar>();
-        
-        ListNode currentNode = head;
-        while (currentNode != null) {
-            departures.add(currentNode.getCalendar());
-            currentNode = currentNode.getNext();
-        }
-        
-        return departures;
-    }
-
-    public void addDeparture(Calendar departure) {
-        ListNode node = new ListNode();
-        node.setCalendar(departure);
-        sortedInsert(node);
-    }
-    
-    private void prepend(ListNode node) throws NullPointerException {
-        
-        if (head == null || tail == null) {
-            head = node;
-            tail = node;
-        } else {
-            head.setPrevious(node);
-            ListNode oldHead = head;
-            head = node;
-            head.setNext(oldHead);
-        }
-    }
-
-    private void append(ListNode node) throws NullPointerException {
-        
-        if (head == null || tail == null) {
-            head = node;
-            tail = node;
-        } else {
-            tail.setNext(node);
-            ListNode oldTail = tail;
-            tail = node;
-            tail.setPrevious(oldTail);
-        }
-    }
-    
-    private void sortedInsert(ListNode node) throws NullPointerException {
-        //TODO  Check if sorted insert really works!!!
-        
-        if (head == null || node.getCalendar().compareTo(head.getCalendar()) < 0) {
-            prepend(node);
-        } else if (node.getCalendar().compareTo(tail.getCalendar()) > 0) {
-            append(node);
-        } else {
-            ListNode pointer = head;
-            ListNode lastPointer = null;
-            while ((node.getCalendar().compareTo(pointer.getCalendar()) > 0) && pointer.getNext() != null) {
-                lastPointer = pointer;
-                pointer = pointer.getNext();
-            }
-            lastPointer.setNext(node);
-            node.setPrevious(lastPointer);
-            node.setNext(pointer);
-            pointer.setPrevious(node);
-        }
-    }
-    
-    // Inner class
-    private class ListNode {
-
-        private Calendar calendar;
-        private ListNode next;
-        private ListNode previous;
-        
-        public ListNode getNext() {
-            return next;
-        }
-        
-        public ListNode getPrevious() {
-            return previous;
-        }
-
-        public void setNext(ListNode next) {
-            this.next = next;
-        }
-        
-        public void setPrevious(ListNode previous) {
-            this.previous = previous;
-        }
-
-        public Calendar getCalendar() {
-            return calendar;
-        }
-
-        public void setCalendar(Calendar calendar) throws NullPointerException {
-            this.calendar = calendar;
-        }
-        
+        return null;
     }
 
 }
