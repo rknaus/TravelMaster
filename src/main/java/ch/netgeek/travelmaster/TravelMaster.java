675b09da68946cab763c9aee641892e1ef3c7ded
Ruben Knaus <ruben.knaus@gmx.ch>
2011-04-21 19:57:57 +0200

    Added the main class

---
+++ src/main/java/ch/netgeek/travelmaster/TravelMaster.java
@@ -0,0 +1,22 @@
+package ch.netgeek.travelmaster;
+
+/**
+ * This is the TravelMaster main program.
+ * TODO: Add general description about the program function here.
+ * 
+ * @author      Dieu P. Van, Ruben Knaus
+ * @version     0.1
+ */
+public class TravelMaster {
+
+    /**
+     * This is the main function of the main program.
+     * 
+     * @param args              Java default arg value
+     */
+    public static void main(String[] args) {
+        // TODO Add method calls and the program initialisation here.
+
+    }
+
+}
