872be7b964d5eaec202c70fff490d98db59332d7
dvan <ph_van@hotmail.com>
2011-04-22 16:01:08 +0200

    modified TransportNetworkTest

--- src/test/java/ch/netgeek/travelmaster/route/TransportNetworkTest.java
+++ src/test/java/ch/netgeek/travelmaster/route/TransportNetworkTest.java
@@ -2,39 +2,62 @@ package ch.netgeek.travelmaster.route;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TransportNetworkTest {
 
-    private TransportNetwork test;
+	private TransportNetwork test;
     private Station stationA = new Station("stationA");
     private Station stationB = new Station("stationB");
     
+    @Before
+    public void setUpTransportNetwork(){
+    	test = new TransportNetwork();
+    }
+    
     @Test
     public void testTransportNetwork() {
-        fail("Not yet implemented");
+        TransportNetwork test = new TransportNetwork();
+        test.getStation("test");
+        test.getLines();
+        test.addConnection(stationA, stationB, 4);
+        assertTrue(test.getStation("test") == null);
+        assertTrue(test.getLines().isEmpty() == true);
     }
 
     @Test
     public void testAddStation() {
         test.addStation("Hollywood");
-        assertTrue(test.getStation("Hollywood")!= null);
+        assertTrue(test.getStation("Hollywood") != null);     
     }
 
     @Test
     public void testAddConnection() {
         test.addConnection(stationA, stationB, 4);
-        Connection connection = new Connection(stationA, stationB, 4);
     }
 
     @Test
     public void testAddLine() {
-        fail("Not yet implemented");
+        TransportNetwork test = new TransportNetwork();
+        ArrayList<Station> stations = new ArrayList<Station>();
+        ArrayList<Calendar> departuresFirstStation = new ArrayList<Calendar>();
+        ArrayList<Calendar> departuresLastStation = new ArrayList<Calendar>();
+        test.addLine(3, "Bus", stations, departuresFirstStation, departuresLastStation);
+        assertTrue(test.getLines() != null);
     }
 
     @Test
     public void testGetStation() {
-        fail("Not yet implemented");
+        test.getStation("Hollywood");
+        Station name = new Station("Hollywood");
+        HashMap<String, Station> stations = new HashMap<String, Station>();
+        stations.put("Hollywood", name);
+        assertTrue(test.getStation("Hollywood") == stations.get(name));
     }
 
     @Test
