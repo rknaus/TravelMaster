82ee016dcc3ee9e15923a5be48ebe361dc3a9f38
Ruben Knaus <ruben.knaus@gmx.ch>
2011-05-07 14:27:00 +0200

    added code comments to RouteCalculator

--- src/main/java/ch/netgeek/travelmaster/algorithm/RouteCalculator.java
+++ src/main/java/ch/netgeek/travelmaster/algorithm/RouteCalculator.java
@@ -23,24 +23,66 @@ import ch.netgeek.travelmaster.route.TransportNetwork;
 public class RouteCalculator {
     
     // class variables declaration
+    
+    /**
+     * Represents the traveling time for all unvisited or unreachable stations.
+     */
     public static final int INFINITE_TRAVEL_TIME = Integer.MAX_VALUE;
     
     // object variables declaration
+    /**
+     * The transport network with all the stations, connection, lines and time
+     * tables for each line and connection.
+     */
     private TransportNetwork transportNetwork;
+    
+    /**
+     * Compares the travel time between two stations
+     */
     private Comparator<Station> shortestTravelTimeComparator;
+    
+    /**
+     * All visited stations
+     */
     private Set<Station> settledStations;
+    
+    /**
+     * All not visited stations
+     */
     private PriorityQueue<Station> unsettledStations;
+    
+    /**
+     * A map of the shortest travel time to each station
+     */
     private Map<Station, Integer> shortestTravelTime;
+    
+    /**
+     * A map of the shortest arrival time of each station
+     */
+    private Map<Station, Calendar> shortestArrivalTime;
+    /**
+     * A map of the predecessor of each station
+     */
     private Map<Station, Station> predecessors;
     
     /**
-     * Initializes the RouteCalculator object
+     * Initializes the RouteCalculator object.<br>
+     * All instance objects get initialized
      * 
      * @param transportNetwork      The instance of the transport network with
      *                              all stations.
      */
     public void RouceCalculator(TransportNetwork transportNetwork) {
         this.transportNetwork = transportNetwork;
+        
+        /*
+         * This comparator compares a station with another one.
+         * If the the travelling time to station b is longer than the travelling
+         * time to the station a it returns -1, other wise it returns 1.
+         * If the travelling time is equal, then it compares the name of the
+         * stations and returns either -1 or 1 too.
+         * Both stations can not have the same name.
+         */
         shortestTravelTimeComparator = new Comparator<Station>() {
 
             @Override
@@ -61,6 +103,7 @@ public class RouteCalculator {
         unsettledStations = new PriorityQueue<Station>(
                 transportNetwork.getStationList().size(), shortestTravelTimeComparator);
         shortestTravelTime = new HashMap<Station, Integer>();
+        shortestArrivalTime = new HashMap<Station, Calendar>();
         predecessors = new HashMap<Station, Station>();
     }
 
@@ -103,6 +146,7 @@ public class RouteCalculator {
             
         }
         
+        //TODO Return correct ArrayList of Stopovers here!!
 		return null;
 	}
     
@@ -115,6 +159,12 @@ public class RouteCalculator {
             if (isSettled(neighbor)) {
                 continue;
             }
+            
+            /*
+             * Add travelling time for each neighbor station to the shortest
+             * travel time map.
+             */
+            
             
             //TODO CONTINUE HERE - This function is not finished yet!
             // int travelTime = getShortestTravelTime(station) + 
