444a471b4812d3126caed690a8acd8d4ed23cf8d
Ruben Knaus <ruben.knaus@gmx.ch>
2011-05-29 15:37:53 +0200

    Added information box for connections to the MapPanel

--- src/main/java/ch/netgeek/travelmaster/gui/MapPanel.java
+++ src/main/java/ch/netgeek/travelmaster/gui/MapPanel.java
@@ -12,6 +12,7 @@ import javax.swing.JPanel;
 
 import ch.netgeek.travelmaster.algorithm.Stopover;
 import ch.netgeek.travelmaster.route.Connection;
+import ch.netgeek.travelmaster.route.Line;
 import ch.netgeek.travelmaster.route.Station;
 
 /**
@@ -48,6 +49,22 @@ public class MapPanel extends JPanel {
         this.stationList = stationList;
         this.connectionList = connectionList;
         this.stopoverList = stopoverList;
+    }
+    
+    /**
+     * Main function which draws the content into the JPanel
+     */
+    @Override
+    protected void paintComponent(Graphics g ) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        Font font = new Font(Font.SANS_SERIF, Font.PLAIN, 12);
+        g2.setFont(font);
+        paintConnections(g2);
+        paintStopoverConnections(g2);
+        paintStations(g2);
+        paintDurationAndLines(g2);
     }
     
     /**
@@ -118,18 +135,56 @@ public class MapPanel extends JPanel {
     }
     
     /**
-     * Main function which draws the content into the JPanel
+     * Draws the duration time of a connection and the lines over the connection
+     * over the middle of the drawn connection line.
+     * 
+     * @param g2                            The GraphicsD2 object
      */
-    @Override
-    protected void paintComponent(Graphics g ) {
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
-                RenderingHints.VALUE_ANTIALIAS_ON);
-        Font font = new Font(Font.SANS_SERIF, Font.PLAIN, 12);
-        g2.setFont(font);
-        paintConnections(g2);
-        paintStopoverConnections(g2);
-        paintStations(g2);
+    private void paintDurationAndLines(Graphics2D g2) {
+        BasicStroke stroke = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
+        g2.setStroke(stroke);
+        for (Connection connection : connectionList) {
+            int duration = connection.getDuration();
+            ArrayList<Line> lineList = connection.getLines();
+            
+            // Creating a String of all line numbers
+            String lines = "";
+            for (Line line : lineList) {
+                if (lines.length() == 0) {
+                    lines = Integer.toString(line.getNumber());
+                } else {
+                    lines = lines + ", " + Integer.toString(line.getNumber());
+                }
+            }
+            
+            /*
+             * Adding a text box in the middle of the connection line with the
+             * duration and line numbers
+             */
+            int xA = connection.getStationA().getXPos() * (panelWidth / 100);
+            int yA = connection.getStationA().getYPos() * (panelHeight / 100);
+            int xB = connection.getStationB().getXPos() * (panelWidth / 100);
+            int yB = connection.getStationB().getYPos() * (panelHeight / 100);
+            
+            /*
+             * The position and size of the connection information (duration and 
+             * line numbers)
+             */
+            int width = 60;
+            int height = 35;
+            int x = (xA + xB) / 2;
+            int y = (yA + yB) / 2;
+            int xRect = x - (width / 2);
+            int yRect = y - (height / 2);
+            int xFont = x - 50;
+            int yFont = y + 5;
+            g2.setColor(Color.GRAY);
+            g2.fillRoundRect(xRect, yRect, width, height, 20, 20);
+            g2.setColor(Color.BLACK);
+            g2.drawRoundRect(xRect, yRect, width, height, 20, 20);
+            g2.drawString(lines, xFont, yFont);
+            
+        }
     }
 
 }
